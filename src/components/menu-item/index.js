1ddf058806e2d68716f1b352483d77393ee0c4e6
pedro <pedro.ciria@totvspartners.com.br>
2020-06-06 17:16:54 -0300

    83783##

---
+++ src/components/menu-item/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./style.scss";
+
+const MenuItem = ({ title, imageUrl, size }) => {
+  return (
+    <div className={`${size} menu-item`}>
+      <div
+        className='background-image'
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+      <div className='content'>
+        <div className='title'>{title.toUpperCase()}</div>
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
+
+MenuItem.defaultProps = {
+  title: "",
+  imageUrl: "",
+  size: "",
+};
