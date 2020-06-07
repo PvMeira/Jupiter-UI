6eefc6df213198521ddbe2d9e8bf385c5c261aaa
pedro <pedro.ciria@totvspartners.com.br>
2020-06-06 23:13:20 -0300

    Adding Route to the code.

--- src/components/menu-item/index.js
+++ src/components/menu-item/index.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
+
 import "./style.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
   return (
-    <div className={`${size} menu-item`}>
+    <div
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
       <div
         className='background-image'
         style={{ backgroundImage: `url(${imageUrl})` }}
@@ -16,7 +21,7 @@ const MenuItem = ({ title, imageUrl, size }) => {
   );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
 
 MenuItem.defaultProps = {
   title: "",
