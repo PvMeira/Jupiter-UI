81eb373f855bc9840bcb5e1cd1f6f6bf97938ea3
pedro <pedro.ciria@totvspartners.com.br>
2020-06-07 00:14:53 -0300

    Creating Basic Header and stufs

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import HomePage from "./screens/home/index";
 import Shop from "./screens/shop/index";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
+import Header from "./components/core/header";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path={"/"} component={HomePage} />
         <Route path={"/shop"} component={Shop} />
